36aa7040ffb51afa2e3d999bc5d65aa61ae815e8
The Tien <tiendt2311@gmail.com>
2017-06-21 19:26:28 +0700

    new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,25 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+   # POST /users
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcom #{@user.name}"
+      session[:user_id] = @user.id
+    else
+      flash[:error] = "Error: #{@user.errors.full_messages.to_sentence}"
+    end
+    redirect_to root_path
+  end
+
+
   def index
     @users = User.all.shuffle
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
