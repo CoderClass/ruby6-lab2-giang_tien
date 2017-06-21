3ce605f85a73c16d55300136748697c76e439115
The Tien <tiendt2311@gmail.com>
2017-06-21 20:39:50 +0700

    Flash message helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,18 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if @user = User.find_by(email: params[:email])
-     if @user.authenticate(params[:password])
+     @user = User.find_by(email: params[:email])
+     if @user && @user.authenticate(params[:password])
        session[:user_id] = @user.id
        flash[:success] = "okok"
        redirect_to root_path
      else
-       render :new
+        flash.now[:error] = "#{@user.errors.messages}"
+        render :new
      end
-   else
-     flash[:error] = "Sign up to continue"
-     redirect_to new_user_path
-   end
   end
 
   def destroy
