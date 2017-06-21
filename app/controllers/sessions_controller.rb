2e288399f90f6f1ebda7ee420dee79ef2ac3b96a
The Tien <tiendt2311@gmail.com>
2017-06-21 19:49:55 +0700

    User login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if @user = User.find_by(email: params[:email])
+     if @user.authenticate(params[:password])
+       session[:user_id] = @user.id
+       flash[:success] = "okok"
+       redirect_to root_path
+     else
+       render :new
+     end
+   else
+     flash[:error] = "Sign up to continue"
+     redirect_to new_user_path
+   end
+  end
+
+  def destroy
+  end
+end
